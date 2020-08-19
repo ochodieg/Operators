1ccd16a14ace62ba64157ae62db0d550984f6d02
Ivan <the_real_iori_orochi@hotmail.com>
2020-08-19 08:55:49 -0600

    Initial Commit

---
+++ Operators.vb
@@ -0,0 +1,29 @@
+﻿'Ivan Ochoa
+'RCET0265
+'Fall 2020
+'Operators Example
+'Repo
+
+
+
+
+Module Operators
+
+    Sub Main()
+        'Let's do math
+        'Comments require " ' " at the start of the comment but not at the end of the comment.
+        Console.WriteLine(2 + 2)
+        Console.ReadLine()
+        Console.WriteLine(7 - 1)
+        Console.ReadLine()
+        Console.WriteLine(5 * 5)
+        Console.ReadLine()
+        Console.WriteLine(7 / 3)
+        Console.ReadLine()
+        Console.WriteLine(7 \ 3)
+        Console.ReadLine()
+        Console.WriteLine(7 Mod 3)
+        Console.ReadLine()
+    End Sub
+
+End Module
