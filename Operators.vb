6b435ee85519c1cbaccc8652cfeb0e45ffed0c78
Ivan <the_real_iori_orochi@hotmail.com>
2020-08-20 08:36:49 -0600

    add compare example

--- Operators.vb
+++ Operators.vb
@@ -24,6 +24,26 @@ Module Operators
         Console.ReadLine()
         Console.WriteLine(7 Mod 3)
         Console.ReadLine()
+        ' concatenate stuff
+        'use with caution
+        'conceatenate (to bring togetyher or aggregate) i used to create
+        'Strings and Is acomplished by quoting the object and using
+        'both "+" And "&" symbols to aggregate the objects
+        Console.WriteLine("2" + "2")
+        Console.ReadLine()
+        Console.WriteLine("Hello" & "," & " " + "world")
+
+        'compare stuff
+        'uses boolean
+        Console.WriteLine(1 > 3)
+        Console.ReadLine()
+        Console.WriteLine(1 < 3)
+        Console.ReadLine()
+        Console.WriteLine(1 = 3)
+        Console.ReadLine()
+        Console.WriteLine(1 <> 3)
+        'not equal
+        Console.ReadLine()
     End Sub
 
 End Module
